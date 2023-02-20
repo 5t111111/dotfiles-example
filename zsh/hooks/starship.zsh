c12a8ad9dfab14fe30ac64cb1b7a200ea4588d58
Hirofumi Wakasugi <baenej@gmail.com>
2023-02-20 18:32:09 +0900

    update

--- zsh/hooks/starship.zsh
+++ zsh/hooks/starship.zsh
@@ -8,7 +8,7 @@ else
   echo -e "WARN: starship is not installed"
 
   if [ "$CODESPACES" = "true" || "$REMOTE_CONTAINERS" = true ] ; then
-    echo -e "INFO: Start Starship installation because it is running on GitHub Codespaces"
+    echo -e "INFO: Start Starship installation because it is running in Codespaces or Remote Containers"
     curl -fsSL https://starship.rs/install.sh -o /tmp/starship_install.sh \
       && chmod 755 /tmp/starship_install.sh \
       && /tmp/starship_install.sh -y
